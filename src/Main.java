f79f3e6c71e65f0d251ea55fe2cc26317dc3ee5f
Andrey Spiridonov <andrejvazapkin@gmail.com>
2022-02-04 01:14:34 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 1500;
+        int miles = price / 20;
+        System.out.println("Начисленно миль за покупку:" + miles);
+    }
+}
